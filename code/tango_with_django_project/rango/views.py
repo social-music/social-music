b40310cb78558f4d48611e7832bf1b2142635241
Mitchell Wolfe <speedywolfe@gmail.com>
2015-04-07 13:12:11 -0600

    added some initial template stuff

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    return render(request, 'rango/index.html', context_dict)
