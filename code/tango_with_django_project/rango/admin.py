0f82f3026773f4a95c6d340642f657561115c4e8
Hannah <imaginationandtech@gmail.com>
2015-04-07 13:49:22 -0600

    Adding Registration Stuff

--- code/tango_with_django_project/rango/admin.py
+++ code/tango_with_django_project/rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Profile
+from rango.models import UserModel
 
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(UserModel)
