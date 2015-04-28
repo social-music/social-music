701b5b82d72154616fc0cee310cd2e4b75d30ecb
Hannah <imaginationandtech@gmail.com>
2015-04-28 15:14:26 -0600

    Updated stream

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -7,13 +7,13 @@ from rango.forms import *
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    content = ContentModel.objects.filter(owner=request.user.profile)
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
 
-    return render(request, 'rango/index.html', context_dict)
+    return render(request, 'rango/index.html', {"content_list": content})
 
 def register(request):
 
